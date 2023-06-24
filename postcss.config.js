a9ec12db7feff87e942dc3f782c4e8dde77b8fbc
ArmyofDeux <103246856+Emmanuetoks@users.noreply.github.com>
2023-06-24 12:16:43 -0700

    make navbar responsive

--- postcss.config.js
+++ postcss.config.js
@@ -2,5 +2,11 @@ module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
+    'postcss-preset-env': {
+      autoprefixer: {grid: false}
+    },
+    'postcss-import': {},
+    'postcss-mixins': {},
+    'postcss-functions': {},
   },
 }
