36cd10914cb38d3a0f8e753222dc94227d4416e9
armyofdeux <103246856+Emmanuetoks@users.noreply.github.com>
2023-07-07 10:59:11 +0100

    Update SecSearchMenu.tsx

--- src/components/Navbar/SecSearchMenu.tsx
+++ src/components/Navbar/SecSearchMenu.tsx
@@ -5,6 +5,8 @@ const SearchMenu = () => {
     ) as HTMLDivElement;
 
     secSearchMenu.classList.remove("navbar__secondary-menu--active");
+const body = document.querySelector('body')
+body.style.overflow = 'auto';
   };
   return (
     <div className="navbar__secondary-search-menu block tablet:hidden bg-black-100">
